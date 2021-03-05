69f7b2135f8186870fce9081ab12cd0e25fd684a
ky <gfdsakmu@gmail.com>
2021-03-06 01:17:17 +0800

    add gamehelper.java

--- ch5/GameHelper.java
+++ ch5/GameHelper.java
@@ -1,5 +1,14 @@
 import java.io.*;
+import java.util.*;
+
 public class GameHelper{
+
+	private static final String alphabet = "abcdefg";
+	private int gridLength = 7;
+	private int gridSize = 49;
+	private int [] grid = new int[gridSize];
+	private int comCount = 0;
+
 	public String getUserInput(String prompt){
 		String inputline = null;
 		System.out.print(prompt + " ");
@@ -10,6 +19,54 @@ public class GameHelper{
 		} catch(IOException e){
 			System.out.println("IOException: " + e);
 		}
-		return inputline;
+		return inputline.toLowerCase();
+	}
+	public ArrayList<String> placeDotCom(int comSize){
+		ArrayList<String> alphacells = new ArrayList<String>();
+		String [] alphacoords = new String[comSize];
+		String temp = null;
+		int [] coords = new int[comSize];
+		int attempts =0;
+		boolean success = false;
+		int locaiton = 0;
+
+		comCount++;
+		int incr = 1;
+		if((comCount%2)==1){
+			incr = gridLength;
+		}
+
+		while(!success&attempts++<200){
+			location = (int)(Math.random()*gridSize);
+			int x = 0;
+			success = true;
+			while(success && x<comSize){
+				if(grid[locaiton] ==0){
+					coords[x++] = locaiton;
+					locaiton += incr;
+					if(locaiton>=gridSize){
+						success = false;
+					}
+					if(x>0 && (location%gridLength==0)){
+						success = false;
+					}
+				}else {
+					success = false;
+				}
+			}
+		}
+		int x = 0;
+		int row = 0;
+		int column = 0;
+		while(x<comSize){
+			grid[coords[x]] = 1;
+			row = (int) (coords[x]/gridLength);
+			column = coords[x]%gridLength;
+			temp = String.valueOf(alphabet.charAt(column));
+
+			alphacells.add(temp.concat(Integer.toString(row)));
+			x++;
+		}
+		return alphacells;
 	}
 }
